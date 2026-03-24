59d14980d550a76e6d91578a6431eda8d539facc
Karen Dayana Alvarez <karendayanat744@gmail.com>
2026-03-24 16:34:43 -0500

    Fix import path for Home view in main.dart

--- Karen Quimbayo - Ejercicio 1/Front/lib/main.dart
+++ Karen Quimbayo - Ejercicio 1/Front/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:proyecto_instructor_diego/ui/views/splash/splash_view.dart';
 import 'package:proyecto_instructor_diego/ui/views/login/Login.dart';
-import 'package:proyecto_instructor_diego/ui/views/Home/Home.dart';
+import 'package:proyecto_instructor_diego/ui/views/home/home.dart';
 
 void main() {
   runApp(const MainApp());
